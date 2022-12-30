0feb8ecddf1fb1ba6bde79716924a0bcc5f176bb
Guilherme Oliveira <francoguilherme.sec@gmail.com>
2022-12-30 12:25:08 -0300

    chore: update login page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,51 +1,71 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
-import {LayoutLogin} from '../../Components/Layout/Login/index'
-
+import styled from "styled-components";
+import { LayoutLogin } from "../../Components/Layout/Login/index";
 
 export const Container = styled.div`
-width: 100%;
-height: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-
-.info{
-  margin-top: 1rem;
-  p{
-    text-transform: uppercase;
-    font-size:0.8rem;
-    color: #566A7F;
-  }
-}
-.LoginForm{
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.8rem;
-  input{
-    width:25vw;
-    height:4vh;
-    border-radius:0.3rem;
-    border: none;
-    background-color: #fff;
+
+  .info {
+    margin-top: 1rem;
+    @media screen and (min-width:1024px){
+      p {
+        text-transform: uppercase;
+        font-size: 0.8em;
+        color: #566a7f;
+      }
+
+    }
+    @media screen and (min-width:768px) and (max-width:1023px){
+      p {
+        text-transform: uppercase;
+        font-size: 0.8em;
+        color: #566a7f;
+      }
+
+    }
+    @media screen and (max-width:767px){
+      p {
+        text-transform: uppercase;
+        font-size: 10px;
+        color: #566a7f;
+      }
+
+    }
   }
-  p{
-    position: relative;
-    text-transform: uppercase;
-    font-size: 0.7rem;
+  .LoginForm {
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.8rem;
+
+      input {
+        width: 25vw;
+        height: auto;
+        border-radius: 0.3rem;
+        border: none;
+        background-color: #fff;
+      }
+      p {
+        position: relative;
+        text-transform: uppercase;
+        font-size: 0.7rem;
+      }
+      button {
+        width: 20vw;
+        height: auto;
+        border-radius: 0.3rem;
+        background-color: #696cff;
+        color: #fff;
+        border: none;
+      }
+    }
   }
-  button{
-    width:20vw;
-    height:4vh;
-    border-radius:0.3rem;
-    background-color: #696cff;
-    color: #fff;
-    border: none;
-  }
-}
-  
 `;
 
 export const Login = () => {
@@ -53,15 +73,21 @@ export const Login = () => {
     <>
       <LayoutLogin>
         <Container>
-          <div className="info"><p>informe suas credenciais de acesso!</p></div>
+          <div className="info">
+            <p>informe suas credenciais de acesso!</p>
+          </div>
           <div className="LoginForm">
-            <input type="text" placeholder="E-mail" />
-            <input type="text" placeholder="Password"/>
-            <p><a href="">esqueci minha senha</a></p>
-            <button type="submit">Login</button>
+            <form action="" className="form">
+              <input type="text" placeholder="E-mail" />
+              <input type="text" placeholder="Password" />
+              <p>
+                <a href="">esqueci minha senha</a>
+              </p>
+              <button type="submit">Login</button>
+            </form>
           </div>
         </Container>
       </LayoutLogin>
     </>
-  )
-}
+  );
+};
