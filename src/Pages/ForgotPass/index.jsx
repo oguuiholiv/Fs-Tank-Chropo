fd0645419c0e22948243bf06fb82c52ba2dfadbd
Guilherme Oliveira <francoguilherme.sec@gmail.com>
2022-12-31 15:51:19 -0300

    chore: update

--- src/Pages/ForgotPass/index.jsx
+++ src/Pages/ForgotPass/index.jsx
@@ -11,7 +11,7 @@ export const Container = styled.div`
   align-items: center;
 
   .content{
-    margin-top:2rem;
+    margin-top:1rem;
     display: flex;
     flex-direction: column;
     align-items: center;
