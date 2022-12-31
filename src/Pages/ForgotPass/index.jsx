a78414d5e06a8d8dfd55d11500c950d0668c27d8
Guilherme Oliveira <francoguilherme.sec@gmail.com>
2022-12-31 18:25:43 -0300

    chore: add media query nas paginas de forgot-pass

--- src/Pages/ForgotPass/index.jsx
+++ src/Pages/ForgotPass/index.jsx
@@ -4,44 +4,132 @@ import { LayoutLogin } from "../../Components/Layout/Login/index";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  .content{
-    margin-top:1rem;
+  @media screen and (min-width: 1027px) {
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    p{
-      text-transform: uppercase;
-      font-size:0.8rem;
-      color: #566A7F;
-    }
-    form{
-      display: flex;
-      margin-top:1rem;
-      flex-direction: column;
-      height: 75%;
-      gap: 2rem;
-      align-items: center;
-      justify-content:space-between;
 
-      input{
-        width:25vw;
-        height:auto;
-        border: none;
-        border-radius: 0.3rem;
+    .content {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      p {
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        color: #566a7f;
       }
-      button{
-        width: 15vw;
-        height: auto;
-        border: none;
-        background-color:#696CFF;
-        color:#FFFFFF;
-        border-radius: 0.3rem;
+      form {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: column;
+        height: 75%;
+        gap: 2rem;
+        align-items: center;
+        justify-content: space-between;
+
+        input {
+          width: 25vw;
+          height: auto;
+          border: none;
+          border-radius: 0.3rem;
+        }
+        button {
+          width: 25vw;
+          height: auto;
+          border: none;
+          background-color: #696cff;
+          color: #ffffff;
+          border-radius: 0.3rem;
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .content {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      p {
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        color: #566a7f;
+      }
+      form {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: column;
+        height: 75%;
+        gap: 2rem;
+        align-items: center;
+        justify-content: space-between;
+
+        input {
+          width: 25vw;
+          height: auto;
+          border: none;
+          border-radius: 0.3rem;
+        }
+        button {
+          width: 25vw;
+          height: auto;
+          border: none;
+          background-color: #696cff;
+          color: #ffffff;
+          border-radius: 0.3rem;
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .content {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      p {
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        color: #566a7f;
+      }
+      form {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: column;
+        height: 75%;
+        gap: 2rem;
+        align-items: center;
+        justify-content: space-between;
+
+        input {
+          width: auto;
+          height: auto;
+          border: none;
+          border-radius: 0.3rem;
+        }
+        button {
+          width: auto;
+          height: auto;
+          border: none;
+          background-color: #696cff;
+          color: #ffffff;
+          border-radius: 0.3rem;
+        }
       }
     }
   }
